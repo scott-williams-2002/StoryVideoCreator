d53a3b543567f496f8e7820affa327758162b4c8
swilliams <smwilliams@mines.edu>
2024-04-05 20:17:34 -0600

    added user input function to select subreddit from pre defined list

--- src/main.py
+++ src/main.py
@@ -1,9 +1,29 @@
 from reddit.Reddit_Handler import grab_posts 
 
 
-posts = grab_posts("offmychest", 3)
+#posts = grab_posts("offmychest", 3)
+def chooseSubreddit(subreddit_list):
+    print(f"Enter a number (0 - {len(subreddit_list)-1}) to select a subreddit")
+    try:
+        for i in range(len(subreddit_list)):
+            print(f"{i} -> {subreddit_list[i]}")
 
-for each in posts:
-    print(each["title"])
-    print("")
-    print(each["content"])
+        user_input = int(input(f"Choice (0 - {len(subreddit_list)-1}): "))
+        print(f"You chose the subreddit: {subreddit_list[user_input]}")
+        return subreddit_list[user_input]
+    except:
+        print("Enter your own subreddit name")
+        subreddit_name = str(input("Name: "))
+        return subreddit_name
+
+
+def main():
+    subreddit_list = ["offmychest","AskMen"]
+    subreddit_name = chooseSubreddit(subreddit_list=subreddit_list)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
